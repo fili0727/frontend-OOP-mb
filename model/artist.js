5c6f0bf734e485b6a664f1592c9980e32eeb5059
TheDanishMexican <danieljchristjansen@gmail.com>
2023-10-09 11:43:12 +0200

    artist object

--- model/artist.js
+++ model/artist.js
@@ -1,6 +1,17 @@
 export default class Artist {
   constructor(obj) {
+    this.id = obj.id;
     this.name = obj.name;
     this.career_start = obj.career_start;
+    this.img = obj.img;
+    this.albums = obj.albums
+  }
+
+  getAlbums() {
+    return this.albums
+  }
+
+  setAlbum(album) {
+    this.albums.push(album.id)
   }
 }
