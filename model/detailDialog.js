0b4f357f228fc06c573f41a26a42d06b4e7f612f
fili0727 <123998888+fili0727@users.noreply.github.com>
2023-10-20 19:47:18 +0200

    update.js

--- model/detailDialog.js
+++ model/detailDialog.js
@@ -3,90 +3,106 @@ import { albums } from "../controller/albumController.js";
 import { readTracks, tracksForArtist } from "../rest.js";
 import Track from "./track.js";
 
-
 export class DetailDialog {
-    constructor(container, object) {
-        this.container = document.querySelector(`#${container}`);
-        this.object = object;
+  constructor(container, object) {
+    this.container = document.querySelector(`#${container}`);
+    this.object = object;
+  }
+
+  showDialog() {
+    this.container.showModal();
+  }
+
+  closeDialog() {
+    this.container.close();
+  }
+
+  clearDialog() {
+    this.container.innerHTML = "";
+  }
+
+  getTracks() {
+    const tracks = [];
+
+    for (const data of tracksForArtist) {
+      const track = new Track(data);
+      tracks.push(track);
     }
+    return tracks;
+  }
 
-    showDialog() {
-        this.container.showModal();
-    }
+  render() {
+    this.clearDialog();
 
-    closeDialog() {
-        this.container.close();
-    }
+    const albumIds = this.object.albums;
+    const artistAlbums = albumIds.map(albumId =>
+      albums.find(album => albumId === album.id)
+    );
 
-    clearDialog() {
-        this.container.innerHTML = '';
-    }
+    const trackIds = [];
+    const artistTracks = [];
 
-    getTracks() {
-        const tracks = [];
+    artistAlbums.forEach(album =>
+      album.tracks.forEach(track => trackIds.push(track))
+    );
 
-        for (const data of tracksForArtist) {
-            const track = new Track(data);
-            tracks.push(track);
-        }
-        return tracks;
-    }
+    trackIds.forEach(trackId => {
+      const found = this.getTracks().filter(track => track.id === trackId);
+      if (found) {
+        artistTracks.push(found);
+      }
+    });
 
-    render() {
-        this.clearDialog();
+    console.log(artistTracks);
 
-        const albumIds = this.object.albums;
-        const artistAlbums = albumIds.map(albumId => albums.find(album => albumId === album.id));
-
-        const trackIds = [];
-        const artistTracks = [];
-
-        artistAlbums.forEach(album => album.tracks.forEach(track => trackIds.push(track)));
-
-        trackIds.forEach(trackId => {
-            const found = this.getTracks().filter(track => track.id === trackId);
-            if (found) {
-                artistTracks.push(found);
-            }
-        });
-
-        console.log(artistTracks);
-
-        const html = /*html*/
-            `<article class="dialog-item">
+    const html =
+      /*html*/
+      `<article class="dialog-item">
                 <h2>${this.object.name}</h2>
                 <h3>Carreer start:</h3>
                 <p>${this.object.career_start}</p>
                 <h3>Appears on albums:</h3>
                 <ul>
-                    ${artistAlbums.map(album => `<li>
+                    ${artistAlbums
+                      .map(
+                        album => `<li>
                         ${album.title}
-                    </li>`).join('')}
+                    </li>`
+                      )
+                      .join("")}
                 </ul>
                 <h3>Appears on tracks:</h3>
-                <img class="detail-dialog-img" src="${this.object.img}" alt='No image'>
+                <img class="detail-dialog-img" src="${
+                  this.object.img
+                }" alt='No image'>
                 <div class="crud-button-container">
-                <button class="crud-button clickable">Update</button>
+                <button class="crud-button clickable update-button">Update</button>
                 <button class="crud-button clickable delete-button">Delete</button>
                 </div>
                 <button class='close-button clickable'>X</button>
             </article>
             `;
 
-        this.container.insertAdjacentHTML('beforeend', html);
+    this.container.insertAdjacentHTML("beforeend", html);
 
-        const closeButton = document.querySelector('.close-button');
+    const closeButton = document.querySelector(".close-button");
 
-        const deleteButton = document.querySelector(".delete-button");
+    const deleteButton = document.querySelector(".delete-button");
 
-        closeButton.addEventListener('click', () => {
-            this.closeDialog();
-        });
+    const updateButton = document.querySelector(".update-button");
 
-        deleteButton.addEventListener("click", () => {
-            deleteArtist(this.object);
-        });
+    closeButton.addEventListener("click", () => {
+      this.closeDialog();
+    });
 
+    deleteButton.addEventListener("click", () => {
+      deleteArtist(this.object);
+    });
 
-    }
+    updateButton.addEventListener("click", () => {
+      console.log(this.object);
+    });
+
+    // updatedArtist(this.object))
+  }
 }
